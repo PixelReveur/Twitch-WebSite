d72dcab187b1ac407f1406098560a309aa3da0c3
PixelReveur <pixelreveur@gmail.com>
2016-03-29 07:30:32 +0200

    Update fr_FR.php

--- includes/languages/fr_FR.php
+++ includes/languages/fr_FR.php
@@ -3,7 +3,7 @@
 $lang_title = "Streamer Twitch";
 $lang_donate = "Donation";
 $lang_isoffline = "n'est pas en live";
-$lang_playing1 = "joue à";
+$lang_playing1 = "joue &agrave;";
 $lang_playing2 = "devant";
 $lang_playing3 = "viewers";
 $lang_viewchannel = "Voir sur Twitch";

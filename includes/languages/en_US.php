7b7c1fda8bbeb791cb74c43be04acf38d8268c92
PixelReveur <pixelreveur@gmail.com>
2018-04-06 23:54:43 +0200

    0.1

--- includes/languages/en_US.php
+++ includes/languages/en_US.php
@@ -5,6 +5,7 @@ $lang_donate = "Donation";
 $lang_isoffline = "is offline";
 $lang_isonline = "is online";
 $lang_playing1 = "playing";
+$lang_playing1B = "playing";
 $lang_playing2 = "with";
 $lang_playing3 = "viewers";
 $lang_viewchannel = "View channel";

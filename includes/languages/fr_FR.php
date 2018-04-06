946be958288726766bd21d254e3427c2b886d396
PixelReveur <pixelreveur@gmail.com>
2018-04-06 23:55:03 +0200

    0.1

--- includes/languages/fr_FR.php
+++ includes/languages/fr_FR.php
@@ -5,6 +5,7 @@ $lang_donate = "Donation";
 $lang_isoffline = "n'est pas en live";
 $lang_isonline = "est en live";
 $lang_playing1 = "joue &agrave;";
+$lang_playing1B = "joue au";
 $lang_playing2 = "devant";
 $lang_playing3 = "viewers";
 $lang_viewchannel = "Voir sur Twitch";
